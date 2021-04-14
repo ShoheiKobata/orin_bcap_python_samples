010225cfa903cf01a767fabd48a10653346984ee
ShoheiKobata <shohei.kobata@denso-wave.co.jp>
2021-04-14 10:16:08 +0900

    Fix 05_00_slvMove.py

--- SimpleSamples/05_00_slvMove.py
+++ SimpleSamples/05_00_slvMove.py
@@ -18,7 +18,7 @@ timeout = 2000
 # If you want to connect to RC8, RC8A, or COBOTTA, select "VRC" as the provider name.
 Name = ""
 Provider = "CaoProv.DENSO.VRC"
-#Provider = "CaoProv.DENSO.VRC9"
+# Provider = "CaoProv.DENSO.VRC9"
 Machine = "localhost"
 Option = ""
 
@@ -32,7 +32,7 @@ print("Send SERVICE_START packet")
 
 # Connect to RC8 (RC8(VRC)provider)
 hCtrl = m_bcapclient.controller_connect(Name, Provider, Machine, Option)
-print("Connect "+Provider)
+print("Connect "+ Provider)
 # get Robot Object Handl
 HRobot = m_bcapclient.controller_getrobot(hCtrl, "Arm", "")
 print("AddRobot")
@@ -62,45 +62,6 @@ Command = "slvSendFormat"
 Param = 0x0000  # Change the format to position
 m_bcapclient.robot_execute(HRobot, Command, Param)
 print("slvMove Format Change" + Command + ":" + str(Param))
-
-# Slave move: Change return format
-print("Open Connection")
-
-# start b_cap Service
-m_bcapclient.service_start("")
-print("Send SERVICE_START packet")
-
-# set Parameter
-Name = ""
-Provider = "CaoProv.DENSO.VRC"
-Machine = ("localhost")
-Option = ("")
-
-# Connect to RC8 (RC8(VRC)provider)
-hCtrl = m_bcapclient.controller_connect(Name, Provider, Machine, Option)
-print("Connect RC8")
-# get Robot Object Handl
-HRobot = m_bcapclient.controller_getrobot(hCtrl, "Arm", "")
-print("AddRobot")
-
-# TakeArm
-Command = "TakeArm"
-Param = [0, 0]
-m_bcapclient.robot_execute(HRobot, Command, Param)
-print("TakeArm")
-
-# Motor On
-Command = "Motor"
-Param = [1, 0]
-m_bcapclient.robot_execute(HRobot, Command, Param)
-print("Motor On")
-
-# Move Initialize Position
-Comp = 1
-Pos_value = [0.0, 0.0, 90.0, 0.0, 90.0, 0.0]
-Pose = [Pos_value, "J", "@E"]
-m_bcapclient.robot_move(HRobot, Comp, Pose, "")
-print("Complete Move P,@E J(0.0, 0.0, 90.0, 0.0, 90.0, 0.0)")
 
 # Slave move: Change return format
 Command = "slvRecvFormat"
@@ -146,28 +107,6 @@ Command = "slvChangeMode"
 # Param = 0x001  # Type P, mode 0 (buffer the destination)
 Param = 0x101  # Type P, mode 1 (overwrite the destination)
 # Param = 0x102  # Type J, mode 1 (overwrite the joint)
-m_bcapclient.robot_execute(HRobot, Command, Param)
-print("slvMove Format Change" + Command + ":" + str(Param))
-
-# Send POS slvMove
-Command = "slvMove"
-LoopNum = 100
-for num in range(LoopNum):
-    Pos_value = [255.0 + num, 0.0, 535.5, 180.0, 0.0, 180.0, 5]
-    ret = m_bcapclient.robot_execute(HRobot, Command, Pos_value)
-    # print(ret)
-    print("time:" + str(ret[0]))
-    print("pos P,J:" + str(ret[1]))
-for num in range(LoopNum):
-    Pos_value = [255.0 + LoopNum - num, 0.0, 535.5, 180.0, 0.0, 180.0, 5]
-    ret = m_bcapclient.robot_execute(HRobot, Command, Pos_value)
-    # print(ret)
-    print("time:" + str(ret[0]))
-    print("pos P,J:" + str(ret[1]))
-
-# Slave move: Change mode
-Command = "slvChangeMode"
-Param = 0x000  # finish Slave Move
 m_bcapclient.robot_execute(HRobot, Command, Param)
 print("slvMove Format Change" + Command + ":" + str(Param))
 
